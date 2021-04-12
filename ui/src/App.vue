1f2bf47f173fd141f29e1e38098f5568e9ae961c
Monty Anderson <work@montyanderson.net>
2021-04-12 17:54:02 +0100

    break prettier

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-	<div id="nav">
-		<router-link to="/">Home</router-link>
-	</div>
-	<router-view />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+  </div>
+<router-view />
 </template>
 
 <style>
