ff7205986c6612f2838a8575f25f65fb37e9d096
Monty Anderson <work@montyanderson.net>
2021-04-13 20:36:13 +0100

    satellite + access + mt code

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -29,20 +29,74 @@
 </style>
 
 <script>
-import { register } from "./lib/satellite.js";
-
-console.log({ register });
+import { AccountAlreadyExistsError, register, login, getProjects, createApiKey } from "./lib/satellite.js";
+import { generateAccess } from "./lib/access.js";
+import { getMtCredentials } from "./lib/mt.js";
 
 export default {
 	async setup() {
-		console.log(
+		const fullName = "Monty Anderson";
+		const shortName = "Monty";
+		const email = "monty+iris-testing-5@storj.io";
+		
+		/*
+		try {
 			await register({
-				fullName: "Monty Anderson",
-				shortName: "Monty",
-				email: "",
-				password: ""
-			})
-		);
+				fullName,
+				shortName,
+				email,
+				password
+			});
+		} catch(error) { 
+			// throw if failed for any other reason than already existing
+			if(!(error instanceof AccountAlreadyExistsError)) {
+				throw error;
+			}
+		}
+		*/
+
+		// get login token
+		const { token } = await login({
+			email,
+			password
+		});
+
+		console.log({token});
+
+		// find primary project
+		const { myProjects: [ project ] } = await getProjects({ token });
+		console.log({ project });
+
+		if(typeof project !== "object") {
+			throw new Error("Couldn't find project");
+		}
+
+		// create new api key for this session
+		// todo: overwrite previous api key
+		const { key } = await createApiKey({
+			token,
+			projectId: project.id,
+			name: `Iris API Key: ${Date.now()}`
+		});
+
+		console.log({ key });
+
+		// create access grant with access to all buckets
+		const { access } = await generateAccess({
+			key,
+			projectId: project.id,
+			satelliteUrl: "12tRQrMTWUWwzwGh18i7Fqs67kmdhH9t6aToeiwbo5mfS2rUmo@us2.tardigrade.io:7777",
+			buckets: [],
+			passphrase: "testestsetset"
+		});
+
+		console.log({ access });
+
+		const { accessKey, secretKey } = await getMtCredentials({
+			access
+		});
+
+		console.log({ accessKey, secretKey });
 	}
 };
 </script>
---
+++ ui/src/lib/mt.js
@@ -0,0 +1,24 @@
+export async function getMtCredentials({ access }) {
+    const response = await fetch("https://auth.tardigradeshare.io/v1/access", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+            access_grant: access,
+            public: false
+        })
+	});
+
+    const {
+        access_key_id,
+        secret_key
+    } = await response.json();
+
+    const credentials = {
+        accessKey: access_key_id,
+        secretKey: secret_key
+    };
+
+    return credentials;
+};
