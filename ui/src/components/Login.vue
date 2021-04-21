aedd170287b442515d867d34415b3bacff767701
Monty Anderson <work@montyanderson.net>
2021-04-21 14:11:32 +0100

    prettierize

--- ui/src/components/Login.vue
+++ ui/src/components/Login.vue
@@ -1,34 +1,32 @@
 <template>
-    <div>
-        <h5 class="mb-4">Login</h5>
+	<div>
+		<h5 class="mb-4">Login</h5>
 
-        <label for="emailAddress">Email Address</label>
-        <input
-            type="email"
-            class="form-control input email"
-            placeholder="example@email.com"
-            v-on:keyup.enter="signUp"
-            id="emailAddress"
-        />
+		<label for="emailAddress">Email Address</label>
+		<input
+			type="email"
+			class="form-control input email"
+			placeholder="example@email.com"
+			v-on:keyup.enter="signUp"
+			id="emailAddress"
+		/>
 
-        <label for="password">Password</label>
-        <input
-            type="password"
-            class="form-control input password"
-            placeholder="••••••••••••"
-            id="password"
-        />
+		<label for="password">Password</label>
+		<input
+			type="password"
+			class="form-control input password"
+			placeholder="••••••••••••"
+			id="password"
+		/>
 
-        <button
-            class="btn btn-primary button signup-btn btn-block"
-        >
-            Login
-        </button>
+		<button class="btn btn-primary button signup-btn btn-block">
+			Login
+		</button>
 
-        <p>
-            Don't have an account?
-            
-            <router-link to="/">Sign Up</router-link>
-        </p>
-    </div>
+		<p>
+			Don't have an account?
+
+			<router-link to="/">Sign Up</router-link>
+		</p>
+	</div>
 </template>
--- ui/src/components/SignUp.vue
+++ ui/src/components/SignUp.vue
@@ -1,60 +1,50 @@
 <template>
-    <div>
-        <h5 class="mb-4">Get Started</h5>
+	<div>
+		<h5 class="mb-4">Get Started</h5>
 
-        <label for="emailAddress">Email Address</label>
-        <input
-            required
-            type="email"
-            class="form-control input email"
-            placeholder="example@email.com"
-            id="emailAddress"
-        />
+		<label for="emailAddress">Email Address</label>
+		<input
+			required
+			type="email"
+			class="form-control input email"
+			placeholder="example@email.com"
+			id="emailAddress"
+		/>
 
-        <label for="password">Password</label>
-        <input
-            required
-            type="password"
-            class="form-control input password"
-            placeholder="••••••••••••"
-            id="password"
-        />
+		<label for="password">Password</label>
+		<input
+			required
+			type="password"
+			class="form-control input password"
+			placeholder="••••••••••••"
+			id="password"
+		/>
 
-        <div
-            class="custom-control custom-checkbox mb-3"
-        >
-            <input
-                required
-                type="checkbox"
-                class="custom-control-input"
-                id="termsCheck"
-            />
-            <label
-                class="custom-control-label"
-                for="termsCheck"
-                >Accept the
-                <a
-                    href="https://tardigrade.io/terms-of-service/"
-                    target="_blank"
-                    >Terms &amp; Conditions</a
-                ></label
-            >
-        </div>
+		<div class="custom-control custom-checkbox mb-3">
+			<input
+				required
+				type="checkbox"
+				class="custom-control-input"
+				id="termsCheck"
+			/>
+			<label class="custom-control-label" for="termsCheck"
+				>Accept the
+				<a
+					href="https://tardigrade.io/terms-of-service/"
+					target="_blank"
+					>Terms &amp; Conditions</a
+				></label
+			>
+		</div>
 
-        <button
-            class="btn btn-primary button btn-block"
-        >
-            Try Storj
-        </button>
+		<button class="btn btn-primary button btn-block">Try Storj</button>
 
-        <hr />
+		<hr />
 
-        <router-link to="/login">
-            <button
-                class="button-no-bg btn btn-success btn-block"
-            >
-                Login to Storj
-            </button>
-        </router-link>
-    </div>
+		<router-link to="/login">
+			<button class="button-no-bg btn btn-success btn-block">
+				Login to Storj
+			</button>
+		</router-link>
+	</div>
 </template>
