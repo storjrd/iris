4beca0bfce05ae13e8a82a29177344169548c040
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-04-23 15:10:16 -0400

    prettierize

--- ui/src/store/buckets.js
+++ ui/src/store/buckets.js
@@ -1,29 +1,29 @@
 export default {
-  state: () => ({
-    names: [],
-    passphrases: {}
-  }),
+	state: () => ({
+		names: [],
+		passphrases: {}
+	}),
 
-  getters: {
-    // is the bucket unlocked?
-    isUnlocked: state => bucket => bucket in state.passphrases,
-    s3: state => bucket => getS3ClientForBucketAndPassphrase()
-  },
+	getters: {
+		// is the bucket unlocked?
+		isUnlocked: (state) => (bucket) => bucket in state.passphrases,
+		s3: (state) => (bucket) => getS3ClientForBucketAndPassphrase()
+	},
 
-  mutations: {
-    // called with the list of all buckets in a user's project
-    setNames(state, names) {
-      state.names = names;
-    },
+	mutations: {
+		// called with the list of all buckets in a user's project
+		setNames(state, names) {
+			state.names = names;
+		},
 
-    // called when the user sets a passphrase
-    unlock(state, { name, passphrase }) {
-      state.passphrases[name] = passphrase;
-    }
-  },
+		// called when the user sets a passphrase
+		unlock(state, { name, passphrase }) {
+			state.passphrases[name] = passphrase;
+		}
+	},
 
-  actions: {
-	  async getBuckets({ dispatch, commit }) {
+	actions: {
+		async getBuckets({ dispatch, commit }) {
 			// get s3 client with access to all buckets [but no passphrase set]
 			const s3 = await dispatch("gateway/getRootClient");
 
@@ -32,8 +32,7 @@ export default {
 			const names = Buckets.map(({ Name }) => Name);
 
 			// update list of buckets
-			commit("setNames, names);
+			commit("setNames", names);
 		}
-  }
-
-}
+	}
+};
