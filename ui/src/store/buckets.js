d07223e27009f16a2cc420774f8227779d70735e
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-04-23 15:09:06 -0400

    Initial buckets store.

---
+++ ui/src/store/buckets.js
@@ -0,0 +1,39 @@
+export default {
+  state: () => ({
+    names: [],
+    passphrases: {}
+  }),
+
+  getters: {
+    // is the bucket unlocked?
+    isUnlocked: state => bucket => bucket in state.passphrases,
+    s3: state => bucket => getS3ClientForBucketAndPassphrase()
+  },
+
+  mutations: {
+    // called with the list of all buckets in a user's project
+    setNames(state, names) {
+      state.names = names;
+    },
+
+    // called when the user sets a passphrase
+    unlock(state, { name, passphrase }) {
+      state.passphrases[name] = passphrase;
+    }
+  },
+
+  actions: {
+	  async getBuckets({ dispatch, commit }) {
+			// get s3 client with access to all buckets [but no passphrase set]
+			const s3 = await dispatch("gateway/getRootClient");
+
+			const { Buckets } = await s3.listBuckets({}).promise();
+
+			const names = Buckets.map(({ Name }) => Name);
+
+			// update list of buckets
+			commit("setNames, names);
+		}
+  }
+
+}
