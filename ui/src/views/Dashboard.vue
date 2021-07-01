eb73c5e3d997cd3a818156cd9ac3db1d479f43d9
Gabriel De Almeida <52146328+gabedealmeida@users.noreply.github.com>
2021-07-01 12:02:56 -0400

    Changed the sidebar to look just like the Figma. (#146)
    * Changed the sidebar to look just like the Figma.
    
    * prettierize

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -58,6 +58,16 @@
 	bottom: 0;
 	margin-top: 330px;
 }
+.project-title {
+	color: white;
+	font-weight: lighter;
+}
+.project-heading {
+	color: white;
+	font-weight: bold;
+	white-space: nowrap;
+	font-size: 0.9rem;
+}
 </style>
 
 <template>
@@ -74,6 +84,35 @@
 					</a>
 
 					<ul class="navbar-nav top">
+						<li class="nav-item">
+							<div class="project-title">Project</div>
+							<a
+								class="
+									nav-link
+									project-heading
+									d-flex
+									justify-content-between
+								"
+								href="javascript:null"
+							>
+								{{ "My Storage Project" }}
+								<span class="align-middle">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-chevron-down"
+										viewBox="0 0 16 16"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+										/>
+									</svg>
+								</span>
+							</a>
+						</li>
 						<li class="nav-item active">
 							<a class="nav-link" href="javascript:null">
 								<div class="nav-icon">
@@ -99,23 +138,24 @@
 							<a class="nav-link" href="javascript:null">
 								<div class="nav-icon">
 									<svg
-										width="18"
-										height="24"
-										viewBox="0 0 18 24"
-										fill="none"
 										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-clock-fill"
+										viewBox="0 0 16 16"
 									>
 										<path
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-											d="M9 23.087C9 23.087 18 13.9706 18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0 13.9706 9 23.087 9 23.087ZM9.0001 12.5217C10.9451 12.5217 12.5218 10.945 12.5218 8.99998C12.5218 7.05498 10.9451 5.47825 9.0001 5.47825C7.0551 5.47825 5.47836 7.05498 5.47836 8.99998C5.47836 10.945 7.0551 12.5217 9.0001 12.5217Z"
-											fill="#768394"
+											d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
 										/>
 									</svg>
 								</div>
-								Endpoint
+								Backup
 							</a>
 						</li>
+					</ul>
+
+					<ul class="navbar-nav bottom pb-4">
 						<li class="nav-item">
 							<a class="nav-link" href="javascript:null">
 								<div class="nav-icon">
@@ -136,31 +176,7 @@
 										/>
 									</svg>
 								</div>
-								Usage
-							</a>
-						</li>
-					</ul>
-
-					<ul class="navbar-nav bottom pb-4">
-						<li class="nav-item">
-							<a class="nav-link" href="javascript:null">
-								<div class="nav-icon">
-									<svg
-										width="18"
-										height="18"
-										viewBox="0 0 18 18"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-											d="M2.18477 6.89616L0.92135 7.11049C0.383646 7.20073 -0.00741218 7.66323 0.000106588 8.20846V9.79149C0.000106588 10.3367 0.391166 10.7992 0.928871 10.8895L2.19229 11.1C2.32014 11.5212 2.4931 11.931 2.69991 12.3258L1.9554 13.3712C1.63954 13.8149 1.69219 14.4203 2.07572 14.8038L3.19625 15.9243C3.40682 16.1349 3.68508 16.2515 3.98213 16.2515C4.21526 16.2515 4.44087 16.18 4.62888 16.0447L5.67045 15.3001C6.06151 15.507 6.47136 15.6762 6.89627 15.8078L7.11059 17.0712C7.20084 17.6089 7.66334 17.9999 8.20857 17.9999H9.79536C10.3406 17.9999 10.8031 17.6089 10.8933 17.0712L11.1039 15.8078C11.5138 15.6799 11.9123 15.5182 12.2921 15.3189L13.3487 16.071C13.5367 16.2063 13.7586 16.2778 13.9917 16.2778C14.2888 16.2778 14.567 16.1612 14.7776 15.9507L15.8981 14.8301C16.2854 14.4466 16.3343 13.8412 16.0184 13.3975L15.2777 12.3672C15.4883 11.9762 15.6612 11.5663 15.7928 11.1452L17.0713 10.9271C17.609 10.8368 18.0001 10.3743 18.0001 9.82909V8.24606C18.0001 7.70083 17.609 7.23833 17.0713 7.14809L15.8229 6.93752C15.6951 6.51262 15.5296 6.10276 15.3228 5.70794L16.0748 4.65133C16.3907 4.20763 16.3381 3.60225 15.9545 3.21871L14.8302 2.09818C14.6234 1.88761 14.3414 1.77104 14.0444 1.77104C13.8112 1.77104 13.5856 1.84248 13.3976 1.97785L12.3636 2.72237C11.9725 2.5118 11.5626 2.33883 11.1415 2.20722L10.9234 0.928763C10.8332 0.391058 10.3707 0 9.82544 0H8.24241C7.69718 0 7.23468 0.391058 7.14443 0.928763L6.93387 2.17714C6.49393 2.30875 6.06903 2.48547 5.66293 2.6998L4.6176 1.95529C4.42959 1.81992 4.20774 1.74848 3.97461 1.74848C3.67756 1.74848 3.39931 1.86505 3.18874 2.07562L2.0682 3.19615C1.6809 3.57968 1.63202 4.18507 1.94788 4.62877L2.69239 5.67034C2.48558 6.0614 2.31637 6.47126 2.18477 6.89616ZM9.01323 6.01634C10.6538 6.01634 11.9838 7.3463 11.9838 8.98688C11.9838 10.6275 10.6538 11.9574 9.01323 11.9574C7.37265 11.9574 6.04269 10.6275 6.04269 8.98688C6.04269 7.3463 7.37265 6.01634 9.01323 6.01634Z"
-											fill="#768394"
-										/>
-									</svg>
-								</div>
-								Settings
+								Plan & Usage
 							</a>
 						</li>
 						<li class="nav-item">
@@ -202,22 +218,46 @@
 							</a>
 						</li>
 						<li v-on:click="logout" class="nav-item">
-							<a class="nav-link" href="#">
-								<div class="nav-icon">
+							<a
+								class="nav-link d-flex justify-content-between"
+								href="#"
+							>
+								<span>
+									<div class="nav-icon">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="16"
+											height="16"
+											fill="currentColor"
+											class="bi bi-person-circle"
+											viewBox="0 0 16 16"
+										>
+											<path
+												d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"
+											/>
+											<path
+												fill-rule="evenodd"
+												d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+											/>
+										</svg>
+									</div>
+									Account
+								</span>
+								<span class="align-middle">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										width="18"
+										width="16"
 										height="16"
-										fill="none"
-										class="bi bi-lock-fill"
-										viewBox="0 0 18 16"
+										fill="currentColor"
+										class="bi bi-chevron-right"
+										viewBox="0 0 16 16"
 									>
 										<path
-											d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"
+											fill-rule="evenodd"
+											d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
 										/>
 									</svg>
-								</div>
-								Logout
+								</span>
 							</a>
 						</li>
 					</ul>
