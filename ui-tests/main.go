abfb4f612145495b9e2225d3c3f53fe47ec49f74
Monty Anderson <work@montyanderson.net>
2021-07-07 16:04:00 +0100

    add 5 second timeout

--- ui-tests/main.go
+++ ui-tests/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"time"
 )
 
 func assertEquals(a string, b string) {
@@ -16,17 +17,23 @@ func assertEquals(a string, b string) {
 func main() {
 	u := launcher.New().Bin("/usr/bin/google-chrome").MustLaunch()
 
-	page := rod.New().ControlURL(u).MustConnect().MustPage("http://ui")
+	page := rod.New().ControlURL(u).MustConnect().MustPage("http://ui").Timeout(5 * time.Second)
 
 	// Test hero title
 
 	title := page.MustElement(".hero-title")
 	assertEquals(title.MustText(), "Decentralized Cloud Storage is Here")
 
-	// Test switch to login box
+	// Must be on registration by default
 	page.MustElementR("h5", "Get Started")
+
+	// Must switch to login
 	page.MustElement("button.btn-success").MustClick()
 	page.MustElementR("h5", "Login")
+
+	// Must switch back
+	page.MustElementR("a", "Sign Up").MustClick()
+	page.MustElementR("h5", "Get Started")
 
 	page.MustWaitLoad().MustScreenshot("/output/screenshot.png")
 }
