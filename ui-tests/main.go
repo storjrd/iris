2d377155817f991ca19504d77f5115f986919b0f
Monty Anderson <work@montyanderson.net>
2021-07-07 18:30:49 +0100

    use repo secrets for Iris login

--- ui-tests/main.go
+++ ui-tests/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"os"
 	"time"
 )
 
@@ -14,13 +15,10 @@ func assertEquals(a string, b string) {
 	}
 }
 
-func main() {
-	u := launcher.New().Bin("/usr/bin/google-chrome").MustLaunch()
-
-	page := rod.New().ControlURL(u).MustConnect().MustPage("http://ui").Timeout(5 * time.Second)
+func testHome(u string) {
+	page := rod.New().ControlURL(u).MustConnect().MustPage("http://ui").Timeout(10 * time.Second)
 
 	// Test hero title
-
 	title := page.MustElement(".hero-title")
 	assertEquals(title.MustText(), "Decentralized Cloud Storage is Here")
 
@@ -34,6 +32,29 @@ func main() {
 	// Must switch back
 	page.MustElementR("a", "Sign Up").MustClick()
 	page.MustElementR("h5", "Get Started")
+}
 
-	page.MustWaitLoad().MustScreenshot("/output/screenshot.png")
+func testLogin(u string) {
+	page := rod.New().ControlURL(u).MustConnect().MustPage("http://ui").Timeout(10 * time.Second)
+
+	// Must switch to login
+	page.MustElement("button.btn-success").MustClick()
+	page.MustElementR("h5", "Login")
+
+	// Login and read bucket names
+	page.MustElement("#emailAddress").MustInput(os.Getenv("IRIS_EMAIL"))
+	page.MustElement("#password").MustInput(os.Getenv("IRIS_PASSWORD"))
+	page.MustElement("button.btn-primary").MustClick()
+	page.MustElement(".bucket-name")
+
+	page.MustWaitLoad().MustScreenshot("/output/login.png")
+}
+
+func main() {
+	u := launcher.New().Bin("/usr/bin/google-chrome").MustLaunch()
+
+	testHome(u)
+
+	testLogin(u)
+
 }
