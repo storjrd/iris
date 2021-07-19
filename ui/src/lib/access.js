7e4030c37592616bd4f442a0a682eade06fdc3e8
Monty Anderson <work@montyanderson.net>
2021-07-19 14:56:20 +0100

    Run wasm inside `WebWorker`

--- ui/src/lib/access.js
+++ ui/src/lib/access.js
@@ -1,70 +1,10 @@
-import "./wasm_exec.js";
+import * as Comlink from "comlink";
+import Worker from "worker-loader!./access.worker.js";
 
-const go = new Go();
+const worker = Comlink.wrap(new Worker());
 
-const wasmInit = WebAssembly.instantiateStreaming(
-	fetch("/access.wasm"),
-	go.importObject
-);
-
-wasmInit.then((result) => {
-	go.run(result.instance);
-});
-
-export async function generateAccess({
-	key,
-	projectId,
-	buckets = [],
-	satelliteUrl = "12tRQrMTWUWwzwGh18i7Fqs67kmdhH9t6aToeiwbo5mfS2rUmo@us2.storj.io:7777",
-	passphrase = ""
-}) {
-	await wasmInit;
-
-	const permission = await global.newPermission();
-
-	if (permission.error) {
-		throw new Error(permission.error);
-	}
-
-	permission.value.AllowDownload = true;
-	permission.value.AllowUpload = true;
-	permission.value.AllowDelete = true;
-	permission.value.AllowList = true;
-
-	console.log(permission);
-
-	const apiKeyPermissionParameters = [
-		key, // api key
-		buckets,
-		permission.value
-	];
-
-	console.log({ apiKeyPermissionParams: apiKeyPermissionParameters });
-
-	const restricted = await global.setAPIKeyPermission(
-		...apiKeyPermissionParameters
-	);
-
-	if (restricted.error) {
-		throw new Error(restricted.error);
-	}
-
-	const accessGrantParameters = [
-		satelliteUrl,
-		restricted.value,
-		passphrase,
-		projectId
-	];
-
-	console.log({ accessGrantParams: accessGrantParameters });
-
-	const access = await global.generateAccessGrant(...accessGrantParameters);
-
-	if (access.error) {
-		throw new Error(access.error);
-	}
-
-	return {
-		access: access.value
-	};
+export async function generateAccess(...args) {
+	 return worker.generateAccess(...args);
 }
+
+export default generateAccess;
