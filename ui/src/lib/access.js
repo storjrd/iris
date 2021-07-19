436d158f15c63aae16a86f0242e9c7c719e1fb54
Monty Anderson <work@montyanderson.net>
2021-07-19 14:57:16 +0100

    prettierize

--- ui/src/lib/access.js
+++ ui/src/lib/access.js
@@ -4,7 +4,7 @@ import Worker from "worker-loader!./access.worker.js";
 const worker = Comlink.wrap(new Worker());
 
 export async function generateAccess(...args) {
-	 return worker.generateAccess(...args);
+	return worker.generateAccess(...args);
 }
 
 export default generateAccess;
