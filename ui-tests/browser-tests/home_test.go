80964b3c57d870789ab33eb281a717c6676beb8d
Gabriel De Almeida <hello@gabrieldealmeida.com>
2021-09-20 13:40:27 -0400

    Refactored testing to mirror storj/storj style of testing.

---
+++ ui-tests/browser-tests/home_test.go
@@ -0,0 +1,30 @@
+package uitest
+
+import (
+	"testing"
+
+	"github.com/go-rod/rod"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHome(t *testing.T) {
+	Browser(t, func (t *testing.T, browser *rod.Browser)  {
+		page := browser.MustPage("http://localhost:3000")
+
+		// Test hero title
+		title := page.MustElement(".hero-title")
+		require.Equal(t, "Decentralized Cloud Storage is Here", title.MustText(), "Home is not being displayed")
+
+		// Must be on registration by default
+		page.MustElementR("h5", "Get Started")
+
+		// Must switch to login
+		page.MustElementR("button", "Login to Storj").MustClick()
+		page.MustElementR("h5", "Login")
+
+		// Must switch back
+		page.MustElementR("a", "Sign Up").MustClick()
+		h5Title := page.MustElementR("h5", "Get Started")
+		require.Equal(t, "Get Started", h5Title.MustText())
+	})
+}
